e853bdf323cd4e8e91c383a528721fbf7cea06e4
Noah <savedream75@gmail.com>
2019-04-06 02:35:39 +0900

    Add Styled-component V4 View

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+          <Button>Hello</Button>
+          <Button active>Hello</Button>
+          <Anchor href="http://google.com">Go to google</Anchor>
+        </Container>
+      </React.Fragment> 
     );
   }
 }
+
+const Container = styled.div`
+  
+`;
+
+const Button = styled.button`
+  color: white;
+  font-size: 50px;
+  padding: 20px 10px;
+  width: 200px;
+  border-radius: 15px;
+  -webkit-appearance: none;
+  cursor: pointer;
+  margin-right: 10px;
+  &:active, &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.active ? 'red' : 'green')}
+`;
+
+const Anchor = styled(Button.withComponent("a"))`
+  text-decoration: none;
+`;
 
 export default App;
