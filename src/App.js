608fc8299e64e6e53eae6165f08133d8fbd2b75e
Noah <savedream75@gmail.com>
2019-04-07 00:23:37 +0900

    Study attrs, css extends

--- src/App.js
+++ src/App.js
@@ -8,15 +8,28 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 5px 10px;
+`;
+
+const Input = styled.input.attrs({
+  required: true,
+})`
+  border-radius: 5px;
+  border: 0;
+  ${awesomeCard}
+`;
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <GlobalStyle />
         <Container>
-          <Button>Hello</Button>
-          <Button active rotationTime={3}>Hello</Button>
-          <Anchor href="http://google.com">Go to google</Anchor>
+          <Input placeholder="hello" />
         </Container>
       </React.Fragment> 
     );
@@ -25,39 +38,7 @@ class App extends Component {
 
 const Container = styled.div`
   background-color: #dedede;
-`;
-
-const Button = styled.button`
-  color: white;
-  font-size: 50px;
-  padding: 20px 10px;
-  width: 200px;
-  border-radius: 15px;
-  -webkit-appearance: none;
-  cursor: pointer;
-  margin-right: 10px;
-  &:active, &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.active ? 'red' : 'green')};
-  ${props => {
-    if(props.active) {
-      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
-    }
-  }};
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg)
-  }
-  to {
-    transform: rotate(360deg);
-  }
+  height: 100vh;
 `;
 
 export default App;
